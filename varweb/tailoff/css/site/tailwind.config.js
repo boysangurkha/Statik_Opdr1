e60a7bc7dd26ee93298ce32a0a717266cfca06c5
statik <statik@statiks-MacBook-Pro.local>
2023-02-20 13:35:56 +0100

    Feat: HERO section is DONE, 1 issue in SERVICE

--- varweb/tailoff/css/site/tailwind.config.js
+++ varweb/tailoff/css/site/tailwind.config.js
@@ -26,6 +26,14 @@ const siteColors = {
 module.exports = {
     content: [`tailoff/js/**/*.{ts,js}`, `templates/_site/**/*`, `templates/jsPlugins/**/*`],
     theme: {
+        borderRadius: {
+            DEFAULT: '4px',
+            'none': '0',
+            'sm': '4px',
+            'md': '8px',
+            'lg': '16px',
+            'full': '9999px',
+        },
         borderWidth: {
             DEFAULT: '1px',
             0: '0',
@@ -73,19 +81,40 @@ module.exports = {
                 logo: '150px',
             },
             fontSize: {
-                'xs': ['0.75rem', '1rem'], // 12px - 16px
-                'sm': ['0.875rem', '1.25rem'], // 14px - 20px
-                'base': ['1rem', '1.5rem'], // 16px - 24px
-                'lg': ['1.125rem', '1.75rem'], // 18px - 28px
-                'xl': ['1.25rem', '1.75rem'], // 20px - 28px
-                '2xl': ['1.5rem', '2rem'], // 24px - 32px
-                '3xl': ['1.875rem', '2.25rem'], // 30px - 36px
-                '4xl': ['2.25rem', '2.5rem'], // 36px - 40px
-                '5xl': ['3rem', '1'], // 48px - 1
-                '6xl': ['3.75rem', '1'], // 60px - 1
-                '7xl': ['4.5rem', '1'], // 72px - 1
-                '8xl': ['6rem', '1'], // 96px - 1
-                '9xl': ['8rem', '1'], // 128px - 1
+                'xxs': ['16px', {
+                    lineHeight: '24px',
+                    fontWeight: '400',
+                }],
+                'xs': ['18px', {
+                    lineHeight: '25px',
+                    fontWeight: '400',
+                }],
+                'sm': ['18px', {
+                    lineHeight: '28px',
+                    fontWeight: '500',
+                }],
+                'md': ['20px', {
+                    lineHeight: '28px',
+                    fontWeight: '500',
+                }],
+
+                'base': ['20px', {
+                    lineHeight: '28px',
+                    fontWeight: '400',
+                }],
+
+                'lg': ['26px', {
+                    lineHeight: '31.21px',
+                    fontWeight: '500',
+                }],
+                'xl': ['32px', {
+                    lineHeight: '38.4px',
+                    fontWeight: '500',
+                }],
+                '2xl': ['44px', {
+                    lineHeight: '49px',
+                    fontWeight: '500',
+                }],
             },
             zIndex: {
                 99: '99',
@@ -112,21 +141,15 @@ module.exports = {
             },
             fontFamily: {
                 'sans': ['FlandersArtSans'],
-                'body': ['FlandersArtSerif'],
+                'serif': ['FlandersArtSerif'],
             },
 
             fontWeight: {
-                thin: '100',
-                hairline: '100',
-                extralight: '200',
-                light: '300',
-                normal: '400',
-                medium: '500',
-                semibold: '600',
-                bold: '700',
-                extrabold: '800',
-                'extra-bold': '800',
-                black: '900',
+                'light': '300',
+                'normal': '400',
+                'medium': '500',
+                'semibold': '600',
+                'bold': '700',
               },
             
         },
